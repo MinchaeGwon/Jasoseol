60667c2d401f08308452476f066be062eedc202a
MinchaeGwon <61741340+MinchaeGwon@users.noreply.github.com>
2020-08-29 02:13:55 +0900

    ModelForm

--- JssProject/urls.py
+++ JssProject/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path
+from main.views import home, create
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home, name="home"),
+    path('create/', create, name="create"),
 ]
